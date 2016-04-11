8f1e1fcf5fb51a86ce14da2fd89a73017a73b8bd
n0x3u5 <nakshatra.mukhopadhyay1994@gmail.com>
2016-04-11 17:24:28 +0530

    Started CricDash for JavaScript project purposes

--- cric_dash/js/cricket.js
+++ cric_dash/js/cricket.js
@@ -1,5 +1,60 @@
+var users = [
+  {
+    "email" : "nox@gmail.com",
+    "password" : "nox123"
+  },
+  {
+    "email" : "ayan@gmail.com",
+    "password" : "ayan123"
+  },
+  {
+    "email" : "prerna@gmail.com",
+    "password" : "prerna123"
+  }
+];
 var submitBtn = document.querySelector('#submit-team');
+var loginBtn = document.querySelector('#login-btn');
 submitBtn.addEventListener("click", function onClick(event) {
   event.preventDefault();
-  $(".card-panel").fadeOut();
+  checkLogin();
+  // $(".card-panel").fadeOut();
 });
+loginBtn.addEventListener("click", function onClick(event) {
+  event.preventDefault();
+  var inputEmail = document.getElementById('email').value;
+  var inputPass = document.getElementById('password').value;
+    if(isValidPassword(inputPass) && isValidUser(inputEmail, inputPass)) {
+      localStorage.setItem('userEmail', inputEmail);
+      localStorage.setItem('isLoggedIn', true);
+      checkLogin();
+      console.log("Is valid email and password!");
+    } else {
+      alert("Invalid user detected. You shall be exterminated.");
+    }
+});
+var checkLogin = function() {
+  if(!localStorage.getItem("isLoggedIn")) {
+    console.log("No one is logged in.");
+    $('#loginModal').openModal({
+      ready: function() { $('ul.tabs').tabs('select_tab', 'login-tab'); }
+    });
+  } else {
+    alert("Now you'll see the magic!");
+  }
+};
+
+var isValidPassword = function(inputPass) {
+
+};
+
+var isValidUser = function(inputEmail, inputPass) {
+  for (var user in users) {
+    if (inputEmail === users[user].email && inputPass === users[user].password) {
+      return true;
+    } else if(user === users.length - 1) {
+      return false;
+    } else {
+      return false;
+    }
+  }
+};
