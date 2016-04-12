364d4a3640965a966bd70f667e504acbb3a6fe6b
n0x3u5 <nakshatra.mukhopadhyay1994@gmail.com>
2016-04-12 13:26:23 +0530

    Login system working as expected. For now.

--- cric_dash/js/cricket.js
+++ cric_dash/js/cricket.js
@@ -12,35 +12,80 @@ var users = [
     "password" : "prerna123"
   }
 ];
+
 var submitBtn = document.querySelector('#submit-team');
 var loginBtn = document.querySelector('#login-btn');
+var signupBtn = document.querySelector('#signup-btn');
+
+window.onload = function() {
+  if (checkLogin()) {
+    createLogout();
+  }
+};
+
 submitBtn.addEventListener("click", function onClick(event) {
   event.preventDefault();
-  checkLogin();
+  if (checkLogin()) {
+
+  } else {
+    createLogin();
+  }
   // $(".card-panel").fadeOut();
 });
 loginBtn.addEventListener("click", function onClick(event) {
   event.preventDefault();
-  var inputEmail = document.getElementById('email').value;
-  var inputPass = document.getElementById('password').value;
-    if(isValidPassword(inputPass) && isValidUser(inputEmail, inputPass)) {
-      localStorage.setItem('userEmail', inputEmail);
-      localStorage.setItem('isLoggedIn', true);
-      checkLogin();
-      console.log("Is valid email and password!");
-    } else {
-      alert("Invalid user detected. You shall be exterminated.");
-    }
-});
-var checkLogin = function() {
-  if(!localStorage.getItem("isLoggedIn")) {
-    console.log("No one is logged in.");
-    $('#loginModal').openModal({
-      ready: function() { $('ul.tabs').tabs('select_tab', 'login-tab'); }
-    });
+  var inputEmail = document.getElementById('loginEmail').value;
+  var inputPass = document.getElementById('loginPassword').value;
+  console.log(inputEmail + " , " + inputPass);
+  if(isValidUser(inputEmail, inputPass)) {
+    localStorage.setItem('userEmail', inputEmail);
+    localStorage.setItem('isLoggedIn', true);
+    createLogout();
+    console.log("Is valid email and password!");
   } else {
-    alert("Now you'll see the magic!");
+    alert("Invalid user detected. You shall be exterminated.");
   }
+});
+signupBtn.addEventListener("click", function onClick(event) {
+  event.preventDefault();
+  var inputEmail = document.getElementById('signupEmail').value;
+  var inputPass = document.getElementById('signupPassword').value;
+  var inputPassConf = document.getElementById('signupPasswordConf').value;
+});
+
+var checkLogin = function() {
+  console.log(localStorage.getItem("isLoggedIn"));
+  if(localStorage.getItem("isLoggedIn") === "false") {
+    console.log("No one is logged in.");
+    return false;
+  } else {
+    console.log("Someone is logged in.");
+    return true;
+  }
+};
+var logoutUser = function() {
+  event.preventDefault();
+  localStorage.setItem('isLoggedIn', false);
+  var logoutA = document.getElementById('logout-anchor');
+  logoutA.parentNode.removeChild(logoutA);
+  Materialize.toast("You have been logged out!", 4000);
+};
+
+var createLogout = function() {
+  var navUl = document.getElementById('screen-nav');
+  var newLi = document.createElement("li");
+  var newA = document.createElement("a");
+  newA.appendChild(document.createTextNode("Logout"));
+  newA.setAttribute("href", "#");
+  newA.setAttribute("id", "logout-anchor");
+  newLi.appendChild(newA);
+  navUl.appendChild(newLi);
+  newA.addEventListener('click', logoutUser);
+};
+var createLogin = function() {
+  $('#loginModal').openModal({
+    ready: function() { $('ul.tabs').tabs('select_tab', 'login-tab'); }
+  });
 };
 
 var isValidPassword = function(inputPass) {
